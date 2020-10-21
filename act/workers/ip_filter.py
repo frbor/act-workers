05d2c5fde2d767f35c7471a3b6678502f6f9f4b2
Fredrik Borg <fredrikb@mnemonic.no>
2020-10-21 10:36:11 +0200

    do not implement as router, only filter

--- act/workers/ip_filter.py
+++ act/workers/ip_filter.py
@@ -19,14 +19,14 @@ PERFORMANCE OF THIS SOFTWARE.
 
 echo the first line and return exit status based on IP type:
 
-0 - Not IP address or public IP address
-1 - No data
-2 - Multicast (RFC 3171, RFC 2373)
-3 - Private (RC 1918, RFC 4193)
-4 - Unspecified (RFC 5735, RFC 2373)
-5 - Reserved (IETF)
-6 - Loopback (RF C3330, RFC 2372)
-7 - Link local (RF3927)
+Filter out all lines if parsed as IP address and IP is
+
+- Multicast (RFC 3171, RFC 2373)
+- Private (RC 1918, RFC 4193)
+- Unspecified (RFC 5735, RFC 2373)
+- Reserved (IETF)
+- Loopback (RF C3330, RFC 2372)
+- Link local (RF3927)
 
 """
 
@@ -40,50 +40,52 @@ from logging import error
 def process() -> None:
     """Read ip addresses from stdin"""
 
-    lines = [line.strip() for line in sys.stdin]
+    output_rows = 0
 
-    if len(lines) > 1:
-        sys.stderr.write(f"Warning: Got {len(lines)} lines, only first line will be used\n")
+    for line in sys.stdin:
+        line = line.strip()
 
-    if not lines[0]:
-        # Empty data
+        if not line:
+            continue
+
+        try:
+            ip = ipaddress.ip_address(line)
+        except ValueError:
+            # No IP Address
+            print(line)
+            output_rows += 1
+            continue
+
+        if ip.is_multicast:
+            #  reserved for multicast use. See RFC 3171 (for IPv4) or RFC 2373 (for IPv6).
+            continue
+
+        elif ip.is_private:
+            # private networks. See RFC 1918 (for IPv4) or RFC 4193 (for IPv6).
+            continue
+
+        elif ip.is_unspecified:
+            # unspecified. See RFC 5735 (for IPv4) or RFC 2373 (for IPv6).
+            continue
+
+        elif ip.is_reserved:
+            # IETF reserved.
+            continue
+
+        elif ip.is_loopback:
+            # loopback address. See RFC 3330 (for IPv4) or RFC 2373 (for IPv6).
+            continue
+
+        elif ip.is_link_local:
+            # link-local usage. See RFC 3927.
+            continue
+
+        print(line)
+        output_rows += 1
+
+    if not output_rows:
+        # No output rows
         sys.exit(1)
-
-    print(lines[0])
-
-    try:
-        ip = ipaddress.ip_address(lines[0])
-    except ValueError:
-        # No IP address
-        sys.exit(0)
-
-    if ip.is_multicast:
-        #  reserved for multicast use. See RFC 3171 (for IPv4) or RFC 2373 (for IPv6).
-        sys.exit(2)
-
-    elif ip.is_private:
-        # private networks. See RFC 1918 (for IPv4) or RFC 4193 (for IPv6).
-        sys.exit(3)
-
-    elif ip.is_unspecified:
-        # unspecified. See RFC 5735 (for IPv4) or RFC 2373 (for IPv6).
-        sys.exit(4)
-
-    elif ip.is_reserved:
-        # IETF reserved.
-        sys.exit(5)
-
-    elif ip.is_loopback:
-        # loopback address. See RFC 3330 (for IPv4) or RFC 2373 (for IPv6).
-        sys.exit(6)
-
-    elif ip.is_link_local:
-        # link-local usage. See RFC 3927.
-        sys.exit(7)
-
-    else:
-        # Other IP address
-        sys.exit(0)
 
 
 def main() -> None:
